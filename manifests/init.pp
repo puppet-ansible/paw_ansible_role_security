b832f5d697370e2de299ac23cb6c3c056726842b
Chris Allen <chris.j.allen1979@gmail.com>
2026-02-02 11:33:30 +0000

    Automated conversion from geerlingguy.security Ansible role

--- manifests/init.pp
+++ manifests/init.pp
@@ -26,6 +26,7 @@
 # @param security_autoupdate_mail_on_error
 # @param security_fail2ban_enabled
 # @param security_fail2ban_custom_configuration_template
+# @param par_vardir Base directory for Puppet agent cache (uses lookup('paw::par_vardir') for common config)
 # @param par_tags An array of Ansible tags to execute (optional)
 # @param par_skip_tags An array of Ansible tags to skip (optional)
 # @param par_start_at_task The name of the task to start execution at (optional)
@@ -63,6 +64,7 @@ class paw_ansible_role_security (
   Boolean $security_autoupdate_mail_on_error = true,
   Boolean $security_fail2ban_enabled = true,
   String $security_fail2ban_custom_configuration_template = 'jail.local.j2',
+  Optional[Stdlib::Absolutepath] $par_vardir = undef,
   Optional[Array[String]] $par_tags = undef,
   Optional[Array[String]] $par_skip_tags = undef,
   Optional[String] $par_start_at_task = undef,
@@ -76,14 +78,13 @@ class paw_ansible_role_security (
   Optional[Boolean] $par_exclusive = undef
 ) {
 # Execute the Ansible role using PAR (Puppet Ansible Runner)
-  $vardir = $facts['puppet_vardir'] ? {
-    undef   => $settings::vardir ? {
-      undef   => '/opt/puppetlabs/puppet/cache',
-      default => $settings::vardir,
-    },
-    default => $facts['puppet_vardir'],
+# Playbook synced via pluginsync to agent's cache directory
+# Check for common paw::par_vardir setting, then module-specific, then default
+  $_par_vardir = $par_vardir ? {
+    undef   => lookup('paw::par_vardir', Stdlib::Absolutepath, 'first', '/opt/puppetlabs/puppet/cache'),
+    default => $par_vardir,
   }
-  $playbook_path = "${vardir}/lib/puppet_x/ansible_modules/ansible_role_security/playbook.yml"
+  $playbook_path = "${_par_vardir}/lib/puppet_x/ansible_modules/ansible_role_security/playbook.yml"
 
   par { 'paw_ansible_role_security-main':
     ensure        => present,
